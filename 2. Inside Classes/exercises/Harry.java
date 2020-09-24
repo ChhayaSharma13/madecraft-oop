5d7fb1d649493c5074bc779e86781dfe86b06919
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-24 17:39:59 +0100

    Exercise-2 -Inside Class

--- 2. Inside Classes/exercises/Harry.java
+++ 2. Inside Classes/exercises/Harry.java
@@ -27,10 +27,15 @@ public class Harry {
 
 	public static void main(String[] args) {
 		// 1. make harry potter
+		Harry Harry = new Harry();
 		// 2. become invisible
+		Harry.makeInvisible(true);
 		// 3. spy on professor snape
+		Harry.spyOnSnape();
 		// 4. become visible again
-		// 5. cast a “stupefy” spell
+		Harry.makeInvisible(false);
+		// 5. cast a â€œstupefyâ€� spell
+		Harry.castSpell("â€œstupefyâ€�");
 	}
 
 }
